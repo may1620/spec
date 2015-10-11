ccf794c57d9662519cdb8b5997ca1dbf9231ed7a
Cody Hanika <codyhanika@gmail.com>
2015-10-11 12:19:56 -0500

    Completed MockStackExchangeRequester for now

--- spec/src/test/java/edu/iastate/cs/design/spec/stackexchange/request/MockStackExchangeRequester.java
+++ spec/src/test/java/edu/iastate/cs/design/spec/stackexchange/request/MockStackExchangeRequester.java
@@ -3,38 +3,48 @@ package edu.iastate.cs.design.spec.stackexchange.request;
 import edu.iastate.cs.design.spec.stackexchange.objects.AnswerDTO;
 import edu.iastate.cs.design.spec.stackexchange.objects.QuestionDTO;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MockStackExchangeRequester implements IStackExchangeRequester {
 
     Map<QuestionDTO, List<AnswerDTO>> postMap;
+    private int nextQuestionId;
+    private int nextAnswerId;
 
     public MockStackExchangeRequester() {
         postMap = new HashMap<QuestionDTO, List<AnswerDTO>>();
+        nextQuestionId = 1;
+        nextAnswerId = 1;
     }
 
     public QuestionDTO postQuestion(QuestionAddRequestData requestData) {
-        QuestionDTO question = new QuestionDTO();
-        // TODO populate question
+        QuestionDTO question = new QuestionDTO(requestData.getTitle(), requestData.getBody(), requestData.getTags(), nextQuestionId);
         postMap.put(question, new ArrayList<AnswerDTO>());
+        ++nextQuestionId;
         return question;
     }
 
     public List<AnswerDTO> getAnswersToQuestion(QuestionAnswersRequestData requestData) {
-        QuestionDTO question = new QuestionDTO();
-        // TODO populate question
+        QuestionDTO question = getQuestionByQuestionId(requestData.getId());
         return postMap.get(question);
     }
 
     public AnswerDTO postAnswerToQuestion(AnswerQuestionRequestData requestData) {
-        QuestionDTO question = new QuestionDTO();
-        AnswerDTO answer = new AnswerDTO();
-        // TODO populate question and answer
+        QuestionDTO question = getQuestionByQuestionId(requestData.getId());
+        // Will throw if question id didn't exist. Fine for the tests.
+        AnswerDTO answer = new AnswerDTO(requestData.getBody(), nextAnswerId, question.getQuestionId());
         List<AnswerDTO> answers = postMap.get(question);
         answers.add(answer);
+        ++nextAnswerId;
         return answer;
+    }
+
+    private QuestionDTO getQuestionByQuestionId(int questionId) {
+        for (QuestionDTO question : postMap.keySet()) {
+            if (question.getQuestionId() == questionId) {
+                return question;
+            }
+        }
+        return null;
     }
 }
