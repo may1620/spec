8f6e0f61542039baa55d3ed66cd3bae9e768abdf
Alex Dana <acdana@iastate.edu>
2015-10-31 22:10:08 -0500

    Put in the functionality to retrieve questions and answers. Still looking into OAuth

--- spec/src/main/java/edu/iastate/cs/design/spec/stackexchange/request/IStackExchangeRequester.java
+++ spec/src/main/java/edu/iastate/cs/design/spec/stackexchange/request/IStackExchangeRequester.java
@@ -3,7 +3,11 @@ package edu.iastate.cs.design.spec.stackexchange.request;
 import edu.iastate.cs.design.spec.stackexchange.objects.AnswerDTO;
 import edu.iastate.cs.design.spec.stackexchange.objects.QuestionDTO;
 
+import java.io.IOException;
 import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
 
 /**
  * This class will provide methods for interacting with StackExchange.
@@ -12,9 +16,9 @@ public interface IStackExchangeRequester {
 
     QuestionDTO postQuestion(QuestionAddRequestData requestData);
 
-    List<AnswerDTO> getAnswersToQuestion(QuestionAnswersRequestData requestData);
+    List<AnswerDTO> getAnswersToQuestion(QuestionAnswersRequestData requestData) throws JSONException, ClientProtocolException, IOException;
 
     AnswerDTO postAnswerToQuestion(AnswerQuestionRequestData requestData);
 
-    QuestionDTO getQuestionByQuestionId(int questionId);
+    QuestionDTO getQuestionByQuestionId(int questionId) throws JSONException, ClientProtocolException, IOException;
 }
--- spec/src/main/java/edu/iastate/cs/design/spec/stackexchange/request/StackExchangeRequester.java
+++ spec/src/main/java/edu/iastate/cs/design/spec/stackexchange/request/StackExchangeRequester.java
@@ -1,23 +1,45 @@
 package edu.iastate.cs.design.spec.stackexchange.request;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import edu.iastate.cs.design.spec.stackexchange.objects.AnswerDTO;
 import edu.iastate.cs.design.spec.stackexchange.objects.QuestionDTO;
-
-import java.util.List;
 
 /**
  * This class will make request to StackExchange. Not a fan of the name right now.
  */
 public class StackExchangeRequester implements IStackExchangeRequester {
 
+	private static CloseableHttpClient httpclient = HttpClientBuilder.create().build();
+	private static HttpGet httpget;
+	private static BasicResponseHandler responseHandler = new BasicResponseHandler();
+	
+	
     public QuestionDTO postQuestion(QuestionAddRequestData requestData) {
         // TODO
         return null;
     }
 
-    public List<AnswerDTO> getAnswersToQuestion(QuestionAnswersRequestData requestData) {
-        // TODO
-        return null;
+    public List<AnswerDTO> getAnswersToQuestion(QuestionAnswersRequestData requestData) throws JSONException, ClientProtocolException, IOException {
+        ArrayList<AnswerDTO> answers = new ArrayList<AnswerDTO>();
+        httpget = new HttpGet(requestData.requestUrl());
+        JSONArray rawAnswers = new JSONObject(httpclient.execute(httpget, responseHandler)).getJSONArray("items");
+        for(int i = 0; i < rawAnswers.length(); i++) {
+        	AnswerDTO answer = new AnswerDTO(rawAnswers.getJSONObject(i).getString("body"), rawAnswers.getJSONObject(i).getInt("answer_id"), requestData.getId());
+        	answers.add(answer);
+        }
+        return answers;
     }
 
     public AnswerDTO postAnswerToQuestion(AnswerQuestionRequestData requestData) {
@@ -25,8 +47,22 @@ public class StackExchangeRequester implements IStackExchangeRequester {
         return null;
     }
 
-    public QuestionDTO getQuestionByQuestionId(int questionId) {
-        // TODO
-        return null;
+    public QuestionDTO getQuestionByQuestionId(int questionId) throws JSONException, ClientProtocolException, IOException {
+    	String questionURL = "http://api.stackexchange.com/2.2/questions/" + questionId + 
+    							"?site=stackoverflow&filter=withbody&sort=votes&key=KW1Do4aYqEdlMNsHpPEHdg((";
+		httpget = new HttpGet(questionURL);
+		JSONObject rawQuestion = new JSONObject(httpclient.execute(httpget, responseHandler));
+		ArrayList<String> tags = new ArrayList<String>();
+		JSONArray rawTags = rawQuestion.getJSONArray("tags");
+		
+		for(int i = 0; i < rawQuestion.getJSONArray("tags").length(); i++) {
+			tags.add(rawTags.getString(i));
+		}
+		
+		String title = rawQuestion.getString("title");
+		String body = rawQuestion.getString("body");
+		QuestionDTO question = new QuestionDTO(title, body, tags, questionId);
+		
+		return question;
     }
 }
