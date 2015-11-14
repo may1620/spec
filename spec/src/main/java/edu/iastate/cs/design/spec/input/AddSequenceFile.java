783800d613fa58dc249b83494d07da5dcae5ce15
Cody Hanika <codyhanika@gmail.com>
2015-11-14 16:34:19 -0600

    Add class for populating our database with a sequence file

---
+++ spec/src/main/java/edu/iastate/cs/design/spec/input/AddSequenceFile.java
@@ -0,0 +1,12 @@
+package edu.iastate.cs.design.spec.input;
+
+/**
+ * This class defines the entry point for the executable which will take a Boa AST sequence file as input and insert
+ * all the methods found into our database.
+ */
+public class AddSequenceFile {
+
+    public static void main(String[] args) {
+        // TODO0
+    }
+}
