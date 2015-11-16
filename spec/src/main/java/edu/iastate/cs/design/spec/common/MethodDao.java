95c2ce7e201e8a1c893ab61f3b2b81adefbda89b
Cody Hanika <codyhanika@gmail.com>
2015-11-16 14:45:22 -0600

    Add concrete Dao classes for Type and Method tables

---
+++ spec/src/main/java/edu/iastate/cs/design/spec/common/MethodDao.java
@@ -0,0 +1,9 @@
+package edu.iastate.cs.design.spec.common;
+
+import edu.iastate.cs.design.spec.entities.Method;
+
+public class MethodDao implements IMethodDao {
+    public void insertMethod(Method method) {
+        // TODO
+    }
+}
---
+++ spec/src/main/java/edu/iastate/cs/design/spec/common/TypeDao.java
@@ -0,0 +1,11 @@
+package edu.iastate.cs.design.spec.common;
+
+
+import edu.iastate.cs.design.spec.entities.Type;
+
+public class TypeDao implements ITypeDao {
+
+    public void insertType(Type type) {
+        // TODO
+    }
+}
