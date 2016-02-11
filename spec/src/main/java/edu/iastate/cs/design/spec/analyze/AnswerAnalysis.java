347c023cb5f62c852ef21f252c485b05d297af73
Cody Hanika <codyhanika@gmail.com>
2016-02-11 13:19:23 -0600

    Commit for demo

--- spec/src/main/java/edu/iastate/cs/design/spec/analyze/AnswerAnalysis.java
+++ spec/src/main/java/edu/iastate/cs/design/spec/analyze/AnswerAnalysis.java
@@ -3,25 +3,30 @@ package edu.iastate.cs.design.spec.analyze;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 import edu.iastate.cs.design.spec.entities.Method;
+import edu.stanford.nlp.international.Language;
+import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
+import edu.stanford.nlp.semgraph.SemanticGraphEdge;
+import edu.stanford.nlp.trees.EnglishGrammaticalRelations;
+import edu.stanford.nlp.trees.GrammaticalRelation;
 import edu.stanford.nlp.util.CoreMap;
 
 public class AnswerAnalysis {
 
     public static void main(String[] args) throws Exception {
         Method testMethod = new Method("testMethod", "void testMethod(String s, int i, List<String> list)", null, 0L);
-        analyzeAnswer(testMethod, "The list will contain the same elements. However, when testMethod runs, the String s will be changed.");
+        analyzeAnswer(testMethod, "The big list will contain the same elements. However, when testMethod runs, the String s will be changed.");
     }
 
     public static void analyzeAnswer(Method method, String answerText) throws IOException {
@@ -33,17 +38,37 @@ public class AnswerAnalysis {
             return;
         }
         Properties nlpProperties = new Properties();
-        nlpProperties.put("annotators", "tokenize, ssplit, pos, lemma, ner, regexner");
+        nlpProperties.put("annotators", "tokenize, ssplit, pos, lemma, ner, regexner, parse, relation");
         nlpProperties.put("regexner.mapping", nerFile.getAbsolutePath());
         StanfordCoreNLP nlpPipeline = new StanfordCoreNLP(nlpProperties);
         Annotation answerAnnotation = new Annotation(answerText);
         nlpPipeline.annotate(answerAnnotation);
         List<CoreMap> sentences = answerAnnotation.get(SentencesAnnotation.class);
+        List<String> paramsFound = new ArrayList<String>();
         for (CoreMap sentence : sentences) {
+            SemanticGraph semanticGraph = sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+            System.out.println(semanticGraph.toString());
             for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+                Set<IndexedWord> adjectives = semanticGraph.getChildrenWithReln(new IndexedWord(token), EnglishGrammaticalRelations.ADJECTIVAL_MODIFIER);
                 String namedEntity = token.get(NamedEntityTagAnnotation.class);
+                Set<IndexedWord> childList = new HashSet<IndexedWord>();
+                for (SemanticGraphEdge edge : semanticGraph.outgoingEdgeIterable(new IndexedWord(token))) {
+                    GrammaticalRelation relation = EnglishGrammaticalRelations.ADJECTIVAL_MODIFIER;
+                    if (edge.getRelation().equals(EnglishGrammaticalRelations.ADJECTIVAL_MODIFIER)) {
+                        childList.add(edge.getTarget());
+                    }
+                }
+                if (namedEntity.equals("PARAM_NAME")) {
+                    paramsFound.add(token.get(CoreAnnotations.TextAnnotation.class));
+
+                    Set<IndexedWord> children = semanticGraph.getChildren(new IndexedWord(token));
+                    for (IndexedWord adjective : adjectives) {
+                        System.out.println(adjective);
+                    }
+                }
             }
         }
+
         nlpPipeline.xmlPrint(answerAnnotation, System.out);
     }
 
@@ -68,11 +93,9 @@ public class AnswerAnalysis {
             nerFileWriter.append(method.getName() + "\tTARGET_METHOD\n");
             // TODO return type?
             for (String paramType : paramTypes) {
-                System.out.println(paramType);
                 nerFileWriter.append(paramType + "\tPARAM_TYPE\n");
             }
             for (String paramName : paramNames) {
-                System.out.println(paramName);
                 nerFileWriter.append(paramName + "\tPARAM_NAME\n");
             }
         } finally {
