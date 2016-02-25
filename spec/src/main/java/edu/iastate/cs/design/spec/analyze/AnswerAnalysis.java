3e4acf1527dfe3564798b8067f2d33003ea484ce
Cody Hanika <codyhanika@gmail.com>
2016-02-25 13:36:13 -0600

    Commit for meeting

--- spec/src/main/java/edu/iastate/cs/design/spec/analyze/AnswerAnalysis.java
+++ spec/src/main/java/edu/iastate/cs/design/spec/analyze/AnswerAnalysis.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.*;
 
 import edu.iastate.cs.design.spec.entities.Method;
-import edu.stanford.nlp.international.Language;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
@@ -17,16 +16,16 @@ import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
-import edu.stanford.nlp.semgraph.SemanticGraphEdge;
-import edu.stanford.nlp.trees.EnglishGrammaticalRelations;
 import edu.stanford.nlp.trees.GrammaticalRelation;
+import edu.stanford.nlp.trees.UniversalEnglishGrammaticalRelations;
 import edu.stanford.nlp.util.CoreMap;
 
 public class AnswerAnalysis {
 
     public static void main(String[] args) throws Exception {
-        Method testMethod = new Method("testMethod", "void testMethod(String s, int i, List<String> list)", null, 0L);
-        analyzeAnswer(testMethod, "The big list will contain the same elements. However, when testMethod runs, the String s will be changed.");
+        //Method testMethod = new Method("testMethod", "void testMethod(String s, int i, List<String> list)", null, 0L);
+        Method testMethod = new Method("randInt", "int randInt(int min, int max)", null, 0L);
+        analyzeAnswer(testMethod, "Returns a pseudo-random number between min and max. Returns a number less than ten. This method returns a number greater than 5.");
     }
 
     public static void analyzeAnswer(Method method, String answerText) throws IOException {
@@ -40,41 +39,237 @@ public class AnswerAnalysis {
         Properties nlpProperties = new Properties();
         nlpProperties.put("annotators", "tokenize, ssplit, pos, lemma, ner, regexner, parse, relation");
         nlpProperties.put("regexner.mapping", nerFile.getAbsolutePath());
+        nlpProperties.put("parse.flags", "-makeCopulaHead");
         StanfordCoreNLP nlpPipeline = new StanfordCoreNLP(nlpProperties);
         Annotation answerAnnotation = new Annotation(answerText);
         nlpPipeline.annotate(answerAnnotation);
         List<CoreMap> sentences = answerAnnotation.get(SentencesAnnotation.class);
-        List<String> paramsFound = new ArrayList<String>();
         for (CoreMap sentence : sentences) {
             SemanticGraph semanticGraph = sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
             System.out.println(semanticGraph.toString());
-            for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
-                Set<IndexedWord> adjectives = semanticGraph.getChildrenWithReln(new IndexedWord(token), EnglishGrammaticalRelations.ADJECTIVAL_MODIFIER);
-                String namedEntity = token.get(NamedEntityTagAnnotation.class);
-                Set<IndexedWord> childList = new HashSet<IndexedWord>();
-                for (SemanticGraphEdge edge : semanticGraph.outgoingEdgeIterable(new IndexedWord(token))) {
-                    GrammaticalRelation relation = EnglishGrammaticalRelations.ADJECTIVAL_MODIFIER;
-                    if (edge.getRelation().equals(EnglishGrammaticalRelations.ADJECTIVAL_MODIFIER)) {
-                        childList.add(edge.getTarget());
-                    }
-                }
-                if (namedEntity.equals("PARAM_NAME")) {
-                    paramsFound.add(token.get(CoreAnnotations.TextAnnotation.class));
+            SentenceClassification classification = classifySentence(sentence);
+            switch (classification) {
+                case NOT_HELPFUL:
+                    System.out.println("Nothing useful was found in this sentence");
+                    break;
+                case PRECONDITION:
+                    break;
+                case POSTCONDITION:
+                    break;
+                case RETURN:
+                    analyzeReturnSentence(sentence);
+                    break;
+                case EXCEPTIONAL:
+                    break;
+            }
 
-                    Set<IndexedWord> children = semanticGraph.getChildren(new IndexedWord(token));
-                    for (IndexedWord adjective : adjectives) {
-                        System.out.println(adjective);
-                    }
+        }
+        nlpPipeline.xmlPrint(answerAnnotation, System.out);
+    }
+
+
+
+    private static void analyzeReturnSentence(CoreMap sentence) {
+        System.out.println("Analyzing return information: " + sentence.get(CoreAnnotations.TextAnnotation.class));
+        SemanticGraph semanticGraph = sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+        IndexedWord root = semanticGraph.getFirstRoot();
+        Set<IndexedWord> deps = semanticGraph.getChildrenWithReln(root, GrammaticalRelation.DEPENDENT);
+
+        IndexedWord dep = null;
+        SpecificationValueType returnType;
+        for (IndexedWord word : deps) {
+            if (isDependentRelevant(word)) {
+                returnType = getReturnType(word, semanticGraph);
+                dep = word;
+            }
+        }
+        Collection<GrammaticalRelation> relevantRelations = new ArrayList<GrammaticalRelation>();
+        relevantRelations.add(UniversalEnglishGrammaticalRelations.NOMINAL_MODIFIER);
+        relevantRelations.add(GrammaticalRelation.DEPENDENT);
+        relevantRelations.add(UniversalEnglishGrammaticalRelations.NUMERIC_MODIFIER);
+        if (dep != null) {
+            Set<IndexedWord> depModifiers = semanticGraph.getChildrenWithRelns(dep, relevantRelations);
+            System.out.println("Modifiers: " + depModifiers.toString());
+            for (IndexedWord modifier : depModifiers) {
+                CoreLabel token = modifier.backingLabel();
+                String ner = token.get(NamedEntityTagAnnotation.class);
+                System.out.println(modifier.toString() + " " + ner);
+            }
+        }
+    }
+
+    private static Map<IndexedWord, NamedEntityTagAnnotation> wordToNerMap(CoreMap sentence) {
+        Map<IndexedWord, NamedEntityTagAnnotation> wordToNerMap = new HashMap<IndexedWord, NamedEntityTagAnnotation>();
+        List<CoreLabel> tokens = sentence.get(TokensAnnotation.class);
+        for (CoreLabel token : tokens) {
+
+        }
+        return wordToNerMap;
+    }
+
+
+    // This function can look at relevant information from the signature and words used in the phrase
+    private static SpecificationValueType getReturnType(IndexedWord word, SemanticGraph graph) {
+        return SpecificationValueType.INTEGER;
+    }
+
+    private enum SpecificationValueType {
+        INTEGER, FLOAT, STRING
+    }
+
+    private class BooleanExpression {
+        private BooleanExpressionType type;
+        private BooleanExpression left;
+        private BooleanExpression right;
+        private ComparisonOperator operator;
+        private int literal;
+
+        public BooleanExpression(BooleanExpressionType type, BooleanExpression left, BooleanExpression right, ComparisonOperator operator, int literal) {
+            this.type = type;
+            this.left = left;
+            this.right = right;
+            this.operator = operator;
+            this.literal = literal;
+        }
+
+        public String toString() {
+            switch (this.type) {
+                case EXPRESSION:
+                    StringBuilder builder = new StringBuilder();
+                    builder.append(left.toString());
+                    builder.append(operator.toString());
+                    builder.append(right.toString());
+                    return builder.toString();
+                case RETURN:
+                    return "\\result";
+                case OLD:
+                    return "\\old";
+                case LITERAL:
+                    return "" + literal;
+                default:
+                    return null;
+            }
+        }
+    }
+
+    private enum BooleanExpressionType {
+        EXPRESSION, RETURN, OLD, LITERAL
+    }
+
+    private static boolean isDependentRelevant(IndexedWord dependent) {
+        return true;
+    }
+
+
+    private enum ComparisonOperator {
+        EQUAL,
+        NOT_EQUAL,
+        LESS_THAN,
+        LESS_THAN_EQUAL,
+        GREATER_THAN,
+        GREATER_THAN_EQUAL,
+        AND,
+        OR;
+
+        public String toString() {
+            switch (this) {
+                case EQUAL:
+                    return "==";
+                case NOT_EQUAL:
+                    return "!=";
+                case LESS_THAN:
+                    return "<";
+                case LESS_THAN_EQUAL:
+                    return "<=";
+                case GREATER_THAN:
+                    return ">";
+                case GREATER_THAN_EQUAL:
+                    return ">=";
+                case AND:
+                    return "&&";
+                case OR:
+                    return "||";
+                default:
+                    return "";
+            }
+        }
+    }
+
+
+    private enum SpecificationType {
+        ENSURES, REQUIRES
+    }
+
+    private enum SentenceClassification {
+        NOT_HELPFUL,
+        PRECONDITION,
+        POSTCONDITION,
+        RETURN,
+        EXCEPTIONAL
+    }
+
+    // TODO currently only looks for return statements
+    private static SentenceClassification classifySentence(CoreMap sentence) {
+        SemanticGraph semanticGraph = sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+        IndexedWord root = semanticGraph.getFirstRoot();
+        if (root.get(CoreAnnotations.TextAnnotation.class).matches("[Rr]eturn+(s)?")) {
+            return SentenceClassification.RETURN;
+        } else {
+            return SentenceClassification.NOT_HELPFUL;
+        }
+    }
+
+    private static class SubjectDirectObjectPair {
+        private String subject;
+        private String directObject;
+
+        public SubjectDirectObjectPair(String subject, String directObject) {
+            this.subject = subject;
+            this.directObject = directObject;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+
+        public String getDirectObject() {
+            return directObject;
+        }
+    }
+
+    private static SubjectDirectObjectPair findNsubjAndDobj(SemanticGraph semanticGraph) {
+        IndexedWord root = semanticGraph.getFirstRoot();
+        Set<IndexedWord> nsubjs = semanticGraph.getChildrenWithReln(root, UniversalEnglishGrammaticalRelations.NOMINAL_SUBJECT);
+        Set<IndexedWord> dobjs = semanticGraph.getChildrenWithReln(root, UniversalEnglishGrammaticalRelations.DIRECT_OBJECT);
+        // TODO if this becomes useful later, actually make this good code
+        if (nsubjs.size() <= 0 || dobjs.size() <= 0) {
+            return new SubjectDirectObjectPair("", "");
+        }
+        Iterator<IndexedWord> iter1 = nsubjs.iterator();
+        Iterator<IndexedWord> iter2 = dobjs.iterator();
+        return new SubjectDirectObjectPair(iter1.next().toString(), iter2.next().toString());
+    }
+
+    private static Set<String> getRelevantAdjectives(CoreMap sentence) {
+        SemanticGraph semanticGraph = sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+        List<String> paramsFound = new ArrayList<String>();
+        Set<String> adjectivesFound = new HashSet<String>();
+        for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+            Set<IndexedWord> adjectives = semanticGraph.getChildrenWithReln(new IndexedWord(token), UniversalEnglishGrammaticalRelations.ADJECTIVAL_MODIFIER);
+            String namedEntity = token.get(NamedEntityTagAnnotation.class);
+            if (namedEntity.equals("PARAM_NAME")) {
+                paramsFound.add(token.get(CoreAnnotations.TextAnnotation.class));
+                for (IndexedWord adjective : adjectives) {
+                    adjectivesFound.add(adjective.get(CoreAnnotations.TextAnnotation.class));
                 }
             }
         }
-
-        nlpPipeline.xmlPrint(answerAnnotation, System.out);
+        return adjectivesFound;
     }
 
     private static File createRegexNerFile(Method method) throws IOException{
         File regexNerFile = File.createTempFile(method.getName(), ".tmp");
-        regexNerFile.deleteOnExit();
+        //regexNerFile.deleteOnExit();
         // extract return type and paramater data from signature
         String signature = method.getSignature();
         String parameterString = signature.substring(signature.indexOf('(') + 1, signature.indexOf(')'));
@@ -90,13 +285,13 @@ public class AnswerAnalysis {
         FileWriter nerFileWriter = null;
         try {
             nerFileWriter = new FileWriter(regexNerFile);
-            nerFileWriter.append(method.getName() + "\tTARGET_METHOD\n");
+            nerFileWriter.append(method.getName() + "\tTARGET_METHOD\r\n");
             // TODO return type?
             for (String paramType : paramTypes) {
-                nerFileWriter.append(paramType + "\tPARAM_TYPE\n");
+                nerFileWriter.append(paramType + "\tPARAM_TYPE\r\n");
             }
             for (String paramName : paramNames) {
-                nerFileWriter.append(paramName + "\tPARAM_NAME\n");
+                nerFileWriter.append(paramName + "\tPARAM_NAME\r\n");
             }
         } finally {
             if (nerFileWriter != null) {
