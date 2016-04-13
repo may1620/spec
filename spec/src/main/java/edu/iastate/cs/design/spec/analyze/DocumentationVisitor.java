2a98516cbd00021a288c9131bf6feea3057804bd
Cody Hanika <codyhanika@gmail.com>
2016-04-12 21:17:54 -0500

    Parsing out all throws documentation, iterates over all util files

---
+++ spec/src/main/java/edu/iastate/cs/design/spec/analyze/DocumentationVisitor.java
@@ -0,0 +1,89 @@
+package edu.iastate.cs.design.spec.analyze;
+
+import org.eclipse.jdt.core.dom.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DocumentationVisitor extends ASTVisitor {
+
+    private boolean classIsPrivate;
+
+    public DocumentationVisitor() {
+        super(true);
+        classIsPrivate = false;
+    }
+
+    @Override
+    public boolean visit(TypeDeclaration node) {
+        if (!nodeIsPublic(node)) {
+            classIsPrivate = true;
+        }
+        return super.visit(node);
+    }
+
+    @Override
+    public void endVisit(TypeDeclaration node) {
+        classIsPrivate = false;
+        super.endVisit(node);
+    }
+
+    @Override
+    public boolean visit(MethodDeclaration node) {
+        if (classIsPrivate || !nodeIsPublic(node)) {
+            return super.visit(node);
+        }
+        Javadoc docNode = node.getJavadoc();
+        if (docNode != null) {
+            for (Object tag : docNode.tags()) {
+                TagElement tagElement = (TagElement) tag;
+                String tagName  = tagElement.getTagName();
+                if (tagName != null && tagName.equals("@throws")) {
+                    List fragments = tagElement.fragments();
+                    String exceptionType = fragments.get(0).toString();
+                    String documentation = "";
+                    for (Object fragment : fragments) {
+                        if (fragment instanceof TextElement) {
+                            documentation += fragment.toString() + " ";
+                        }
+                    }
+                    analyzeThrows(exceptionType, documentation.trim(), node);
+                }
+            }
+        }
+        return super.visit(node);
+    }
+
+    private void analyzeThrows(String exceptionType, String documentation, MethodDeclaration node) {
+        List<SingleVariableDeclaration> params = node.parameters();
+        List<String> paramTypes = new ArrayList<String>();
+        List<String> paramNames = new ArrayList<String>();
+        for (SingleVariableDeclaration param : params) {
+            paramTypes.add(param.getType().toString());
+            paramNames.add(param.getName().toString());
+        }
+        // do some analysis, maybe make a new class for this?
+    }
+
+
+    private boolean nodeIsPublic(BodyDeclaration node) {
+        List a = node.modifiers();
+        for (Object o : a) {
+            if (o instanceof Modifier) {
+                Modifier mod = (Modifier) o;
+                if (mod.getKeyword().toString().equals("public")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+    @Override
+    public boolean visit(Javadoc node) {
+        return super.visit(node);
+    }
+
+}
---
+++ spec/src/main/java/edu/iastate/cs/design/spec/analyze/UtilTest.java
@@ -0,0 +1,47 @@
+package edu.iastate.cs.design.spec.analyze;
+
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+
+public class UtilTest {
+
+    public static void main(String[] args) {
+        File srcDir = new File("C:\\Users\\chanika\\Desktop\\src\\java\\util");
+        for (File file : srcDir.listFiles()) {
+            if (file.isFile()) {
+                testFile(file);
+            }
+        }
+    }
+
+    public static void testFile(File javaSourceFile) {
+        System.out.println(javaSourceFile.getName());
+        if (!javaSourceFile.getName().equals("ArrayList.java"))
+            return;
+        ASTParser parser = ASTParser.newParser(AST.JLS8);
+        parser.setKind(ASTParser.K_COMPILATION_UNIT);
+        parser.setResolveBindings(true);
+        try {
+            Scanner scanner = new Scanner(javaSourceFile).useDelimiter("\\Z");
+            String fileString = scanner.next();
+            parser.setSource(fileString.toCharArray());
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Error reading file! Exiting.");
+            return;
+        }
+        Map options = JavaCore.getOptions();
+        JavaCore.setComplianceOptions(JavaCore.VERSION_1_8, options);
+        parser.setCompilerOptions(options);
+
+        CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+        DocumentationVisitor visitor = new DocumentationVisitor();
+        cu.accept(visitor);
+    }
+}
