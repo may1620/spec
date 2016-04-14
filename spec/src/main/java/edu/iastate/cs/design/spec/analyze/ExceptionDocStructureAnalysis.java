9a8027b98abf0eabc2c711b8af5008ca917b9998
Cody Hanika <codyhanika@gmail.com>
2016-04-14 01:49:41 -0500

    Add class for finding common structures in throws comments

---
+++ spec/src/main/java/edu/iastate/cs/design/spec/analyze/ExceptionDocStructureAnalysis.java
@@ -0,0 +1,12 @@
+package edu.iastate.cs.design.spec.analyze;
+
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+
+import java.util.List;
+
+public class ExceptionDocStructureAnalysis implements ExceptionDocProcessor{
+
+    public void process(String exceptionType, String documentation, List<String> paramTypes, List<String> paramNames, MethodDeclaration methodNode) {
+
+    }
+}
